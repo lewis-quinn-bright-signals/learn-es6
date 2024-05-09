8c33f15dceee9e8332121852f90637f45466df90
Lewis Quinn <lewis.quinn@brightsignals.co.uk>
2024-05-09 17:10:31 +0100

    UPDATE: Lesson 3 init

---
+++ script.js
@@ -0,0 +1,12 @@
+let receive = (complete = () => 
+    console.log("complete") || displayInPreview("complete")) => complete()
+
+receive();
+
+// display in plunker preview
+function displayInPreview(string) {
+    var newDiv = document.createElement("div"); 
+    var newContent = document.createTextNode(string); 
+    newDiv.appendChild(newContent);
+    document.body.appendChild(newDiv)
+}
