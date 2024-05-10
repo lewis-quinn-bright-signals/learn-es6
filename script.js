295419de667d02f3466c14a51572181470b6f8c4
Lewis Quinn <lewis.quinn@brightsignals.co.uk>
2024-05-10 13:34:03 +0100

    UPDATE: Lesson 6 init

---
+++ script.js
@@ -0,0 +1,27 @@
+var color = "red";
+var speed = 10;
+var drive = "go";
+var car = {
+    color,
+    speed,
+    [drive]: function(){
+        console.log("vroom") || displayInPreview("vroom");
+    }
+};
+
+
+
+console.log(car.color) || displayInPreview(car.color);
+console.log(car.speed) || displayInPreview(car.speed);
+
+car[drive]();
+
+
+
+// display in plunker preview
+function displayInPreview(string) {
+  var newDiv = document.createElement("div"); 
+  var newContent = document.createTextNode(string); 
+  newDiv.appendChild(newContent);
+  document.body.appendChild(newDiv)
+}
